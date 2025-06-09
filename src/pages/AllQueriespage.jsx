89fe689a7b44a3a760a7eb009866757cb25a643a
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2025-06-09 16:52:25 +0600

    all-queries page designed

--- src/pages/AllQueriespage.jsx
+++ src/pages/AllQueriespage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Queries from '../components/Queries/Queries';
 
 const AllQueriespage = () => {
     return (
         <div>
-            All Queries
+            <Queries/>
         </div>
     );
 };
